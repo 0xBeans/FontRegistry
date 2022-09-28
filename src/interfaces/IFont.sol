282e791d9b9bec15cd5b59534a1375ea95cbd18e
beans <certifiedbagholder@gmail.com>
2022-09-28 11:42:51 -0400

    comments

--- src/interfaces/IFont.sol
+++ src/interfaces/IFont.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+/// @title The interface on-chain font contracts must implement to be added to the registry.
+///
+/// Uploading fonts to chain is open ended and up to the dev (SSTORE2 or hardcoded string or etc).
+///
+/// As long as the font contract implements this interface and has immutable font data, it can be added
+/// to the registry.
+///
+/// @author @0x_beans
 interface IFont {
     // address that uploaded font for credits
     function fontUploader() external returns (address);
@@ -20,6 +28,6 @@ interface IFont {
     // necessary to differentiate uploaded fonts that are the same but different style
     function fontStyle() external returns (string memory);
 
-    // return the full base64 encoded font with `data:font/ttf;charset=utf-8;base64,` prefix
+    // return the full base64 encoded font with data uri scheme prefix (`data:font/ttf;charset=utf-8;base64,`)
     function getFont() external view returns (string memory);
 }
